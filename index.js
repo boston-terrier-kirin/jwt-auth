ff274df612afebf16e64ab9029c1737ec8972ecb
kirin <kohei.matsumoto@gmail.com>
2021-06-12 16:03:03 +0900

    moving mongoUri to config

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const config = require('./config');
 const router = require('./router');
 const app = express();
 
-mongoose.connect(
-	'mongodb+srv://emaily:HdioBJr8eFHM75WU@cluster0.h3ozz.mongodb.net/auth?retryWrites=true&w=majority',
-	{
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true,
-	}
-);
+mongoose.connect(config.mongoUri, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
 
 app.use(morgan('combined'));
 app.use(express.json());
